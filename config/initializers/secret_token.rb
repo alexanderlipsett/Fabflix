c9b10788c1db5a8b2301e5d73d2102787a431060
Neal Young <neal.young@me.com>
2013-02-18 17:38:13 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabflix::Application.config.secret_token = '949b8ed4246a0881f9f91b7531db2eeb924c54b1fa35084d84d0459790d8b60074137ff6072c063d2f45bf77c86a5dd6557999e6d9aa88f533e0a6d66c4e8f0e'
